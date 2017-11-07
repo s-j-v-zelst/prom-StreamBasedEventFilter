051f1cf586fb67d9e8469a26019fb306adbcdaaf
mfanisan <mfanisan@nbwin1443.campus.tue.nl>
2017-11-07 10:27:10 +0100

    Adding two parameters:1- adjusting method 2- filtering method

--- src/org/processmining/streambasedeventfilter/parameters/ConditionalProbabilitiesBasedXSEventFilterParametersImpl.java
+++ src/org/processmining/streambasedeventfilter/parameters/ConditionalProbabilitiesBasedXSEventFilterParametersImpl.java
@@ -3,7 +3,8 @@ package org.processmining.streambasedeventfilter.parameters;
 public class ConditionalProbabilitiesBasedXSEventFilterParametersImpl extends XSEventFilterParametersImpl {
 
 	private int maxPatternLength = 3;
-
+	private FilterMethod filtermethod= FilterMethod.Forward;
+	private AdjustmentDistribution adjustdistribute = AdjustmentDistribution.MaxNZAbg;
 	private double cutoffThreshold = 0.01;
 
 	public int getMaxPatternLength() {
@@ -20,6 +21,22 @@ public class ConditionalProbabilitiesBasedXSEventFilterParametersImpl extends XS
 
 	public void setCutoffThreshold(double cutoffThreshold) {
 		this.cutoffThreshold = cutoffThreshold;
+	}
+
+	public FilterMethod getFiltermethod() {
+		return filtermethod;
+	}
+
+	public void setFiltermethod(FilterMethod filtermethod) {
+		this.filtermethod = filtermethod;
+	}
+
+	public AdjustmentDistribution getAdjustdistribute() {
+		return adjustdistribute;
+	}
+
+	public void setAdjustdistribute(AdjustmentDistribution adjustdistribute) {
+		this.adjustdistribute = adjustdistribute;
 	}
 
 }
